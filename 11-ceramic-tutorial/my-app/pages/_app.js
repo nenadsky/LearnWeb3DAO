7ac18af73136290de85e93a460c173bbb1326268
nenadsky <nenad.dimitrovski@gmail.com>
2023-10-04 23:33:40 +0200

    added 11th and 12th projects

---
+++ 11-ceramic-tutorial/my-app/pages/_app.js
@@ -0,0 +1,13 @@
+import { Provider } from '@self.id/react';
+
+import '@/styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
+}
+
+export default MyApp;
